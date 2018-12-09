9c7573c443158e3f15a610450aaa9e859f3eb936
Marianne Feng <feng.marianne@gmail.com>
2018-12-09 14:10:28 -0800

    stubbed out backend methods

--- back/stalker.go
+++ back/stalker.go
@@ -5,14 +5,33 @@ import (
 "strings"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	message := r.URL.Path
-	message = strings.TrimPrefix(message, "/")
-	message = "Hello " + message
-	w.Write([]byte(message))
+func getAllContainers(w http.ResponseWriter, r *http.Request) {
+	//returns a list of docker containers
 }
+
+func login(w http.ResponseWriter, r *http.Request) {
+	//POST call
+	//this takes in a json body: { "password" : xxxxx }
+	//the passed body is compared against environment variable PASSWORD set on backend startup
+	//returns true/false auth
+}
+
+func isSecure(w http.ResponseWriter, r *http.Request) {
+	//GET call
+	//returns true/false for whether or not there is a login
+	//if PASSWORD is set, then true
+}
+
+
+func restartContainer(w http.ResponseWriter, r *http.Request) {
+	//POST call
+}
+
 func main() {
-	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/containers", getAllContainers)
+	http.HandleFunc("/restart", restartContainer)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/isSecure", isSecure)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
