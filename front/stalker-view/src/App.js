dfee4f96a2cd15ed315395ae0223660692b32ca4
Jessi Shank <jessi@ten-forward.com>
2018-12-10 00:44:49 -0800

    changed title to be the minty color, idk if i like it

--- front/stalker-view/src/App.js
+++ front/stalker-view/src/App.js
@@ -4,13 +4,13 @@ import ContainerTable from './Components/ContainerTable'
 import ContainerDetail from './Components/ContainerDetail'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-
+import './Utils/Common.scss'
 
 class App extends Component {
   render() {
     return (
         <div>
-            <h1 className="App-header">docker stalker</h1><hr className="rule"/>
+            <h1 className="App-header stalker-color">docker stalker</h1><hr className="rule"/>
             <Router>
                 <div className="App">
                     <Route exact path='/' render={()=><Auth />} />
